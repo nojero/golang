041c6dfff994c09b47c36d4fb805d1b3de66d250
Santiago Gonzalez <nojero@gmail.com>
2017-12-14 17:15:47 -0300

    Changes to computer

--- computer/computer.go
+++ computer/computer.go
@@ -3,66 +3,71 @@ package computer
 import "fmt"
 
 type instruction struct {
-    ins string
-    arg int
+	ins string
+	arg int
 }
 
-type computer struct {
-    pc int
-    sp int
-    size int
-    stack []instruction
+type Computer struct {
+	pc    int
+	sp    int
+	size  int
+	stack []instruction
 }
 
-func New(size int) computer {
-    return computer{0,0,size,make([]instruction,size)}
+func New(size int) Computer {
+	return Computer{0, 0, size, make([]instruction, size)}
 }
 
-func (c *computer) SetAddress(addr int) {
-    if c.pc < c.size {
-        c.pc = addr
-    }
+func (c *Computer) SetAddress(addr int) {
+	if c.pc < c.size {
+		c.pc = addr
+	}
 }
 
-func (c *computer) Insert(ins string, arg int) {
-    instr := instruction{ins, arg}
-    c.stack[c.pc] = instr
-    c.pc += 1
-    if c.sp < c.pc && c.pc < c.size {
-        c.sp = c.pc
-    }
+func (c *Computer) Insert(ins string, arg int) {
+	instr := instruction{ins, arg}
+	c.stack[c.pc] = instr
+	c.pc += 1
+	if c.sp < c.pc && c.pc < c.size {
+		c.sp = c.pc
+	}
 }
 
-func (c *computer) Execute() {
-    for {
-        i := c.stack[c.pc]
-        switch i.ins {
-        case "MULT":
-            a := c.stack[c.sp - 1]
-            b := c.stack[c.sp - 2]
-            c.stack[c.sp - 2] = instruction{"", a.arg * b.arg}
-            c.sp -= 1
-            c.pc += 1
-        case "PUSH":
-            c.stack[c.sp] = instruction{"", i.arg}
-            c.sp += 1
-            c.pc += 1
-        case "PRINT":
-            fmt.Println(c.stack[c.sp - 1].arg)
-            c.sp -= 1
-            c.pc += 1
-        case "STOP":
-            return
-        case "RET":
-            c.pc = c.stack[c.sp - 1].arg
-            c.sp -= 1
-        case "CALL":
-            c.pc = i.arg
-        default:
-            fmt.Println("Instruction not supported: ", i.ins)
-            return
-        }
-
-    }
+func (c *Computer) Print() {
+	for i := 0; i < c.size; i++ {
+		fmt.Println(i, c.stack[i].ins, c.stack[i].arg)
+	}
 }
 
+func (c *Computer) Execute() (string, error) {
+	ret := ""
+	for {
+		i := c.stack[c.pc]
+		switch i.ins {
+		case "MULT":
+			a := c.stack[c.sp-1]
+			b := c.stack[c.sp-2]
+			c.stack[c.sp-2] = instruction{"", a.arg * b.arg}
+			c.sp -= 1
+			c.pc += 1
+		case "PUSH":
+			c.stack[c.sp] = instruction{"", i.arg}
+			c.sp += 1
+			c.pc += 1
+		case "PRINT":
+			ret = fmt.Sprintf("%s\n%s", ret, c.stack[c.sp-1].arg)
+			c.sp -= 1
+			c.pc += 1
+		case "STOP":
+			return ret, nil
+		case "RET":
+			c.pc = c.stack[c.sp-1].arg
+			c.sp -= 1
+		case "CALL":
+			c.pc = i.arg
+		default:
+			return "", fmt.Errorf("Instruction not supported: %s", i.ins)
+		}
+
+	}
+}
