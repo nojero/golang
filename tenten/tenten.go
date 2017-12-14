f68ffc52972f043c03cc99dd22962e34c5521f56
Santiago Gonzalez <nojero@gmail.com>
2017-12-14 17:13:26 -0300

    go fmt

--- tenten/tenten.go
+++ tenten/tenten.go
@@ -6,8 +6,8 @@ import (
 	"github.com/julienschmidt/httprouter"
 	"github.com/nojero/computer"
 	"io/ioutil"
-	"net/http"
 	"log"
+	"net/http"
 	"strconv"
 )
 
@@ -24,7 +24,7 @@ func parseBody(r *http.Request, t interface{}) error {
 
 	json.Unmarshal([]byte(body), t)
 
-    return nil
+	return nil
 }
 func createComputer(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	type Args struct {
@@ -32,13 +32,13 @@ func createComputer(w http.ResponseWriter, r *http.Request, _ httprouter.Params)
 	}
 	var args Args
 
-    err := parseBody(r, &args)
-    if err != nil{
+	err := parseBody(r, &args)
+	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
+		return
+	}
 
-    defer r.Body.Close()
+	defer r.Body.Close()
 
 	size := args.Stack
 	if size <= 0 {
@@ -53,95 +53,95 @@ func createComputer(w http.ResponseWriter, r *http.Request, _ httprouter.Params)
 }
 
 func setAddress(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-    strId := ps.ByName("id")
+	strId := ps.ByName("id")
 
-    id, err := strconv.Atoi(strId)
-    if err != nil {
+	id, err := strconv.Atoi(strId)
+	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
+		return
+	}
 
-    // valid id
+	// valid id
 	type Args struct {
 		Addr int `json:"addr"`
 	}
 	var args Args
 
-    err = parseBody(r, &args)
-    if err != nil{
+	err = parseBody(r, &args)
+	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
+		return
+	}
 
-    defer r.Body.Close()
-    if id < len(computers) {
-        comp := &computers[id]
-        comp.SetAddress(args.Addr)
-    } else {
+	defer r.Body.Close()
+	if id < len(computers) {
+		comp := &computers[id]
+		comp.SetAddress(args.Addr)
+	} else {
 		http.Error(w, "Computer not existant", http.StatusInternalServerError)
-    }
+	}
 }
 
 func insert(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-    strId := ps.ByName("id")
-    ins := ps.ByName("type")
+	strId := ps.ByName("id")
+	ins := ps.ByName("type")
 
-    id, err := strconv.Atoi(strId)
-    if err != nil {
+	id, err := strconv.Atoi(strId)
+	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
+		return
+	}
 
-    // valid id
+	// valid id
 	type Args struct {
 		Arg int `json:"arg"`
 	}
 	var args Args
 
-    err = parseBody(r, &args)
-    if err != nil{
+	err = parseBody(r, &args)
+	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
+		return
+	}
 
-    defer r.Body.Close()
+	defer r.Body.Close()
 
-    if id < len(computers) {
-        comp := &computers[id]
-        comp.Insert(ins, args.Arg)
-    } else {
+	if id < len(computers) {
+		comp := &computers[id]
+		comp.Insert(ins, args.Arg)
+	} else {
 		http.Error(w, "Computer not existant", http.StatusInternalServerError)
-    }
+	}
 }
 
 func execute(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-    strId := ps.ByName("id")
+	strId := ps.ByName("id")
 
-    id, err := strconv.Atoi(strId)
-    if err != nil {
+	id, err := strconv.Atoi(strId)
+	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
+		return
+	}
 
-    if id < len(computers) {
-        comp := &computers[id]
-        ret, err := comp.Execute()
-        if err != nil {
-	        fmt.Fprintf(w, ret)
-        } else {
-		    http.Error(w, err.Error(), http.StatusInternalServerError)
-        }
-    } else {
+	if id < len(computers) {
+		comp := &computers[id]
+		ret, err := comp.Execute()
+		if err != nil {
+			fmt.Fprintf(w, ret)
+		} else {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	} else {
 		http.Error(w, "Computer not existant", http.StatusInternalServerError)
-    }
+	}
 }
 
 func main() {
 	router := httprouter.New()
 	router.POST("/v1/computers", createComputer)
-    router.PATCH("/v1/computers/:id/stack/pointer", setAddress)
-    router.POST("/v1/computers/:id/stack/insert/:type", insert)
-    router.POST("/v1/computers/:id/exec", execute)
+	router.PATCH("/v1/computers/:id/stack/pointer", setAddress)
+	router.POST("/v1/computers/:id/stack/insert/:type", insert)
+	router.POST("/v1/computers/:id/exec", execute)
 
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
